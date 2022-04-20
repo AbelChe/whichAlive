38d74f034008caf39adef64e00db2b7c53fb271d
AbelChe <40518242+AbelChe@users.noreply.github.com>
2022-04-20 17:56:07 +0800

    Revert "0.1.2 bug fix"

--- whichalive.py
+++ whichalive.py
@@ -6,7 +6,6 @@ import re
 import socket
 import time
 import urllib
-import urllib.parse
 from concurrent.futures import ALL_COMPLETED, ThreadPoolExecutor, wait
 
 import requests
@@ -19,12 +18,10 @@ class whichAlive(object):
     def __init__(self, file, THREAD_POOL_SIZE=10, allow_redirect=False, PROXY={}):
         self.file = file
         self.filename = ''.join(file.split('/')[-1].split('.')[:-1])
-        self.timenow = str(time.time()).split(".")[0]
-        self.outfilename = f'{self.filename}{self.timenow}.csv'
-        self.errorfilename = f'error_{self.filename}{self.timenow}.txt'
+        self.outfilename = f'{self.filename}{str(time.time()).split(".")[0]}.csv'
         self.urllist = self.__urlfromfile()
         self.tableheader = ['no', 'url', 'ip', 'state',
-                            'state_code', 'title', 'server', 'length', 'other']
+                            'state_code', 'title', 'server', 'length']
         self.HEADER = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
         }
@@ -48,25 +45,15 @@ class whichAlive(object):
             print(f'--------------------------------\nDONE, use {(end_time - start_time).seconds} seconds')
             print(f'outfile: {os.path.join(os.path.abspath(os.path.dirname(__file__)), "result", self.outfilename)}')
 
-    def __scan(self, url, no, tryagainflag=False):
-        def callback(no, url, ip, state, state_code, title, server, length, other):
-            self.completedurl += 1
-            thisline = [no, url, ip, state, state_code, title, server, length, other]
-            nowpercent = '%.2f'%((self.completedurl/self.allurlnumber)*100)
-            print(f'[{nowpercent}%] {url} {ip} {state} {title} {length}')
-            self.__writetofile(thisline)
-
-        ip = ''
-        state = ''
+    def __scan(self, url, no):
         state_code = -1
         title = ''
-        server = ''
         length = -1
-        other = ''
         try:
-            if DEBUG: print(f'[+] {no} {url}')
+            if DEBUG:
+                print(f'[+] {no} {url}')
             u = urllib.parse.urlparse(url)
-            ip = self.__getwebip(u.netloc.split(':')[0])
+            ip = self.__getwebip(u.netloc)
             if self.allow_redirect:
                 r = requests.get(url=url, headers=self.HEADER, timeout=15, verify=False, proxies=self.PROXY)
                 state = 'alive'
@@ -81,28 +68,17 @@ class whichAlive(object):
                 title = self.__getwebtitle(r)
                 length = self.__getweblength(r)
                 server = self.__getwebserver(r)
-            callback(no, url, ip, state, state_code, title, server, length, other)
-        except requests.exceptions.ConnectTimeout as e:
-            if DEBUG: print(f'[ConnectTimeout] {url} {e}')
-            self.__errorreport(str(e))
+        except requests.exceptions.ConnectTimeout:
             state = 'dead'
-            callback(no, url, ip, state, state_code, title, server, length, 'ConnectTimeout')
-        except requests.exceptions.ReadTimeout as e:
-            if DEBUG: print(f'[ReadTimeout] {url} {e}')
-            self.__errorreport(str(e))
+        except requests.exceptions.ReadTimeout:
             state = 'dead'
-            callback(no, url, ip, state, state_code, title, server, length, 'ReadTimeout')
-        except requests.exceptions.ConnectionError as e:
-            if DEBUG: print(f'[ConnectionError] {url} {e}')
-            self.__errorreport(str(e))
+        except requests.exceptions.ConnectionError:
             state = 'dead'
-            callback(no, url, ip, state, state_code, title, server, length, 'ConnectionError')
-        except Exception as e:
-            if DEBUG: print(f'[ERROR] {no} {url} {e}')
-            self.__errorreport(str(e))
-            if TRYAGAIN and not tryagainflag:
-                self.__scan(url, no, True)
-            callback(no, url, ip, state, state_code, title, server, length, 'e')
+        self.completedurl += 1
+        thisline = [no, url, ip, state, state_code, title, server, length]
+        nowpercent = '%.2f'%((self.completedurl/self.allurlnumber)*100)
+        print(f'[{nowpercent}%] {url} {ip} {state} {title} {length}')
+        self.__writetofile(thisline)
 
     def __getwebtitle(self, r):
         try:
@@ -145,11 +121,6 @@ class whichAlive(object):
         writer.writerow(data)
         f.close()
 
-    def __errorreport(self, message):
-        f = open(f'error/{self.errorfilename}', 'a')
-        f.write(message+'\n')
-        f.close()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage='whichAlive usage')
@@ -157,11 +128,9 @@ if __name__ == '__main__':
     parser.add_argument('--proxy', default='', help='Set proxy, such as 127.0.0.1:8080')
     parser.add_argument('-t', '--thread', default=10, type=int, help='Set max threads, default 10')
     parser.add_argument('-d', '--debug', default=False, action='store_true', help='print some debug information')
-    parser.add_argument('--try-again', default=False, action='store_true', help='If some error, try again scan that url once', dest='tryagain')
     args = parser.parse_args()
 
     DEBUG = args.debug
-    TRYAGAIN = args.tryagain
 
     w = whichAlive(
         file=args.file,
@@ -170,3 +139,4 @@ if __name__ == '__main__':
         PROXY={'http': args.proxy, 'https': args.proxy}
     )
     w.run()
+
